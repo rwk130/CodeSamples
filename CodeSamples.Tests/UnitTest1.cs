9367884018e067e2185bd16aa2239ae5d79af3d3
Richard W Kraemer <genetic_mistake@hotmail.com>
2017-09-26 02:06:31 -0400

    Add project files.

---
+++ CodeSamples.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeSamples.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
